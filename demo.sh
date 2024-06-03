abc0c86c20e38ddbf6d3baa803f2fa6139616fc8
mengyou2 <85054155+mengyou2@users.noreply.github.com>
2024-06-03 20:06:53 +0800

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,21 +1,29 @@
 export CUDA_VISIBLE_DEVICES=5
 
-# ##dynamic image
-
-DIR_PATH=/home/youmeng/youmeng/diffusers_final/example_imgs/dynamic
-
-python svd_interpolate_dyn_img.py --folder_path "$DIR_PATH" --iteration 1 --major_radius 15 --minor_radius 30 --degrees_per_frame 1.0 --lr 0.02 --weight_clamp 0.6
-# python svd_interpolate_dyn_img.py --folder_path "$DIR_PATH" --iteration 3 --major_radius 15 --minor_radius 30 --degrees_per_frame 1.0 --inverse True --lr 0.02 --weight_clamp 0.6
-
-
-# ####multi image
-# python svd_interpolate_two_img.py --lr 0.02 --scene scan1 --src1 23 --src2 26 --dataset dtu
-
 
 ###single image
 
+### path to image
+IMG_PATH=example_imgs/single/000001.jpg
+### path to data
+DIR_PATH=example_imgs/single/
+### rotate the camera to right 
+python svd_interpolate_single_img.py --image_path "$IMG_PATH" --folder_path "$DIR_PATH" --iteration 3 --inverse True  --major_radius 25 --minor_radius 23 --degrees_per_frame 1.0 --lr 0.02 --weight_clamp 0.2
+### rotate the camera to left 
+python svd_interpolate_single_img.py --image_path "$IMG_PATH" --folder_path "$DIR_PATH" --iteration 1 --major_radius 25 --minor_radius 23 --degrees_per_frame 1.0 --lr 0.02 --weight_clamp 0.2
 
-# IMG_PATH=/home/youmeng/youmeng/diffusers_final/example_imgs/single/000001.jpg
-# DIR_PATH=/home/youmeng/youmeng/diffusers_final/example_imgs/single/
-# python svd_interpolate_single_img.py --image_path "$IMG_PATH" --folder_path "$DIR_PATH" --iteration 3 --inverse True  --major_radius 25 --minor_radius 23 --degrees_per_frame 1.0 --lr 0.02 --weight_clamp 0.2
-# python svd_interpolate_single_img.py --image_path "$IMG_PATH" --folder_path "$DIR_PATH" --iteration 1 --major_radius 25 --minor_radius 23 --degrees_per_frame 1.0 --lr 0.02 --weight_clamp 0.2
+
+### dynamic setting
+### path to data
+DIR_PATH=example_imgs/dynamic
+
+### rotate the camera to right 
+python svd_interpolate_dyn_img.py --folder_path "$DIR_PATH" --iteration 1 --major_radius 15 --minor_radius 30 --degrees_per_frame 1.0 --lr 0.02 --weight_clamp 0.6
+### rotate the camera to left 
+python svd_interpolate_dyn_img.py --folder_path "$DIR_PATH" --iteration 3 --major_radius 15 --minor_radius 30 --degrees_per_frame 1.0 --inverse True --lr 0.02 --weight_clamp 0.6
+
+
+#### spare setting
+python svd_interpolate_two_img.py --lr 0.02 --scene scan1 --src1 23 --src2 26 --dataset dtu
+
+
